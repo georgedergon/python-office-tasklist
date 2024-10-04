ee2ce6946883350b6590588c52acc1e953877c1a
george$ <georgedergon@gmx.de>
2024-10-04 17:24:41 +0200

    the end

--- python-office-tasklist.py
+++ python-office-tasklist.py
@@ -7,7 +7,7 @@ def add_task():
        print(f"The task {task} was added to your list.")
     else:
         print("Your tasklist ist empty.")
-add_task()
+
 
 def show_tasklist():
     if office_tasklist == None:
@@ -16,8 +16,22 @@ def show_tasklist():
         for task in office_tasklist:
             print("Your tasklist:")
             print(task)
-show_tasklist
+show_tasklist()
 
 def main():
     while True:
-        print("\")
+        print("1. Add a task")
+        print("2. Show the list.")
+        print("3. End the programm.")
+        choice = input("Please make your choice.")
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            show_tasklist()
+        elif choice == "3":
+            print("Good Bye.")
+            break
+
+
+if __name__=="__main__":
+    main()
